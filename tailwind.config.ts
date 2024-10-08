ee5fb1e5f01fabc41275de389a0a4d46dc969679
lokajaya1 <lokajayandala1@gmail.com>
2024-10-08 18:13:13 +0700

    setup projects

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,8 +9,9 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#1B2FFF",
+        secondary: "#F5F8FE",
+        dark: "#171B20",
       },
     },
   },
