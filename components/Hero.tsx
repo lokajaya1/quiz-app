e22f6bd1a81c7169a859e59ad05269fae6ffda01
lokajaya1 <lokajayandala1@gmail.com>
2024-10-10 18:51:31 +0700

    hero section

---
+++ components/Hero.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="relative w-full min-h-[500px] flex items-center justify-center text-center">
+      <div className="px-4 md:px-6 max-w-[1500px] mx-auto w-[90%]">
+        <div className="space-y-2">
+          <h1 className="text-4xl font-bold tracking-tighter sm:text-5xl md:text-5xl lg:text-5xl/none text-dark">
+            Ready to take this week&apos;s quiz ?
+          </h1>
+          <p className="text-gray-600">Get ready to ace it</p>
+        </div>
+        <div className="mt-6">
+          <Link
+            href={"/quiz"}
+            className="inline-flex items-center justify-center rounded-md bg-primary px-8 py-3 text-sm font-medium text-gray-50 shadow transition-colors hover:bg-primary/80"
+          >
+            I&apos;m ready mf
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
